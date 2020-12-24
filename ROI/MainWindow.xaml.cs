d862cdf5d50265ee92b64101935e265475e9e743
zhu daokuan <15549519416@163.com>
2020-12-24 11:23:07 +0800

    add GDI+ Project

---
+++ ROI/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ROI
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ROICtrl_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.Source is ROICtrl)
+            {
+                //ROICtrl img = (ROICtrl)e.OriginalSource;
+                //鼠标捕获此图片
+                //roi.CaptureMouse();
+                roi.IsSelected = true;
+
+                roi.InvalidateVisual();
+                //SetOtherUnSelect(img);
+            }
+            else
+            {
+                //roi.CaptureMouse();
+                roi.IsSelected = false;
+
+                roi.InvalidateVisual();
+                //SetOtherUnSelect(img);
+            }
+        }
+    }
+}
---
+++ ROI/ROICtrl.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ROI
+{
+    /// <summary>
+    /// ROI.xaml 的交互逻辑
+    /// </summary>
+    public partial class ROICtrl : UserControl
+    {
+        public bool IsSelected { get; set; }
+        public ROICtrl()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnRender(DrawingContext dc)
+        {
+            base.OnRender(dc);
+            if (IsSelected)
+            {
+                try
+                {
+                    //绘制边框
+                    Rect rect = new Rect(0, 0, this.ActualWidth, this.ActualHeight);
+                    Pen p = new Pen(new SolidColorBrush(Colors.Red), 2);
+                    p.DashStyle = DashStyles.Dot;
+                    p.StartLineCap = PenLineCap.Triangle;
+                    dc.DrawRectangle(Brushes.Transparent, p, rect);
+
+                    ////在4个角画出对应的表示方向的小方块
+                    //double area = 20;
+                    //dc.DrawRectangle(Brushes.Red, p, new Rect(0, 0, area, area));
+                    //dc.DrawRectangle(Brushes.Red, p, new Rect(this.ActualWidth - area, 0, area, area));
+                    //dc.DrawRectangle(Brushes.Red, p, new Rect(0, this.ActualHeight - area, area, area));
+                    //dc.DrawRectangle(Brushes.Red, p, new Rect(this.ActualWidth - area, this.ActualHeight - area, area, area));
+                }
+                catch { }
+            }
+        }
+
+        private void UserControl_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
